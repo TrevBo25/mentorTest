913bc054639e032a2876ae419b2eca549c758a69
Trevbo25 <trevorbrown25@gmail.com>
2017-10-31 00:11:05 -0700

    donezo

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,84 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt: function (str){
+    return str.split('').reverse().join('');
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups: function(arr){
+    return [ ...new Set(arr)]
+  },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt: function (str){
+    let taco = str.split(' ');
+    let cat = '';
+    for (let i = 0; i<taco.length; i++){
+      if(i !== (taco.length - 1)){
+        for ( let j = 0; j<taco[i].length; j++){
+          if( typeof taco[i][j] === 'string'){
+            if(j === 0){
+              cat += taco[i][j].toUpperCase();
+            } else {
+              cat += taco[i][j].toLowerCase();
+            }
+          } else {
+            cat += taco[i][j].toString();
+          }
+        }
+        cat += " ";
+      } else {
+          for ( let j = 0; j<taco[i].length; j++){
+          if( typeof taco[i][j] === 'string'){
+            if(j === 0){
+              cat += taco[i][j].toUpperCase();
+            } else {
+              cat += taco[i][j].toLowerCase();
+            }
+          } else {
+            cat += taco[i][j].toString();
+          }
+        }
+      }
+    }
+    return cat;
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
-  //capitalize the first letter of each word
+  //capitalize the first letter of each word tacocat
 
-  // vowelCounter:
+  vowelCounter: function(str){
+    let winner = 0;
+    str.split('').forEach((v,i,a) => {
+      if('aeiou'.includes(v)){
+        winner++;
+      }
+    })
+    return winner;
+  },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime: function (n){
+    if ( n < 0 ){
+      return false
+    }
+    var arr = [];
+    for (i = 2; i <= n / 2; i++) {
+      n % i === 0 ? arr.push(i) : (arr = arr);
+    }
+    if (arr.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +87,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: "1020",
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +98,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+  log1: "Hello World",
+  log2: undefined
 }
